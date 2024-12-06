ab82f19e11b6ce538b050e0184608c89053b0531
balabandhavi <115206578+balabandhavi@users.noreply.github.com>
2024-12-06 22:33:01 +0530

    fq

---
+++ WeirdAlgorithm.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextInt();
+        while(n!=1){
+            System.out.print(n+" ");
+            if(n%2==0){
+                n/=2;
+            }else{
+                n=(n*3)+1;
+            }
+        }
+        System.out.print(1);
+        sc.close();
+    }
+}
